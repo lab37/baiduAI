3d79af3ea5264583fdd2dd85055ff32ae490f45b
NATA <379718363@qq.com>
2019-04-26 07:19:06 +0800

    V1.0.1

--- main.go
+++ main.go
@@ -1,138 +1,96 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-	"net/http"
-	"log"
 	"errors"
-	"strings"
-	"github.com/axgle/mahonia"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
 )
 
-// 词法分析
+const (
+	BAIDU_AI_APPID = 16114169
+	BAIDU_AI_KEY   = "GSE6LEdGFvhSFf7vSVC5TIGD"
+	BAIDU_AI_CRET  = "sIzN7VRya9hT82NpqGwp67Lbcf7sfaYj"
+	API_TIMEOUT    = 120
+)
 
-type Config struct {
-	ApiKey    string // client id
-	SecretKey string // client secret
-}
+const (
+	TOKEN_API_URI = "https://openapi.baidu.com/oauth/2.0/token"
 
-type AccessTokenResponse struct {
+	// 文本相关API
+	SEG_API_URI       = "https://aip.baidubce.com/rpc/2.0/nlp/v1/wordseg"
+	WORDPOS_API_URI   = "https://aip.baidubce.com/rpc/2.0/nlp/v1/wordpos"
+	WORDEMBED_API_URI = "https://aip.baidubce.com/rpc/2.0/nlp/v1/wordembedding"
+	DNNL_API_URI      = "https://aip.baidubce.com/rpc/2.0/nlp/v1/dnnlm_cn"
+	SIMNET_API_URI    = "https://aip.baidubce.com/rpc/2.0/nlp/v1/simnet"
+	COMTAG_API_URI    = "https://aip.baidubce.com/rpc/2.0/nlp/v1/comment_tag"
+
+	// 语音相关API
+	TXT2VOICE_API_URI = "http://tsn.baidu.com/text2audio" // 语音合成
+	VOICE2TXT_API_URI = "http://vop.baidu.com/server_api" // 语音识别
+
+	// OCR相关API
+	IDCARD_API_URI     = "https://aip.baidubce.com/rest/2.0/ocr/v1/idcard"
+	BANKCARD_API_URI   = "https://aip.baidubce.com/rest/2.0/ocr/v1/bankcard"
+	GENERALOCR_API_URI = "https://aip.baidubce.com/rest/2.0/ocr/v1/general"
+	TABLE_API_REQ_URI  = "https://aip.baidubce.com/rest/2.0/solution/v1/form_ocr/request" //表格识别异步端口
+	TABLE_API_RESP_URI = "https://aip.baidubce.com/rest/2.0/solution/v1/form_ocr/get_request_result"
+	// 人脸检测相关API
+	FACEDETECT_API_URI = "https://aip.baidubce.com/rest/2.0/face/v1/detect"
+	FACEMATCH_API_URI  = "https://aip.baidubce.com/rest/2.0/faceverify/v1/match"
+
+	// 黄反识别
+	ANTIPORN_API_URI = "https://aip.baidubce.com/rest/2.0/antiporn/v1/detect"
+)
+
+type accessTokenResponse struct {
 	ExpiresIn        int64  `json:"expires_in"`        // 过期时间
 	AccessToken      string `json:"access_token"`      // 访问码
 	Error            string `json:"error"`             // 错误码
 	ErrorDescription string `json:"error_description"` // 错误信息
 }
 
-func getToken(cfg *Config) (string, error) {
-	var b bytes.Buffer
-	b.WriteString("https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=")
-	b.WriteString(cfg.ApiKey)
-	b.WriteString("&client_secret=")
-	b.WriteString(cfg.SecretKey)
-	res, err := http.Post(b.String(), "application/json; charset=UTF-8", nil)
-	if err != nil {
-		return "", err
-	}
-	defer res.Body.Close()
+var currentAccessToken = new(accessTokenResponse)
 
-	ret := new(AccessTokenResponse)
-	if err = json.NewDecoder(res.Body).Decode(ret); err != nil {
-		return "", err
-	}
-    
-	if ret.Error != "" {
-		return "", errors.New(ret.Error + ": " + ret.ErrorDescription)
+func getToken() *accessTokenResponse {
+	postArgs := url.Values{
+		"client_id":     {BAIDU_AI_KEY},
+		"client_secret": {BAIDU_AI_CRET},
+		"grant_type":    {"client_credentials"},
 	}
 
-	return ret.AccessToken, nil
-}
-
-
-func analyzeWords(tk string, ex string ) (*wordResult, error) {
-	enc:=mahonia.NewEncoder("gbk")
-	str:=enc.ConvertString(ex)
-	var a bytes.Buffer
-	a.WriteString("{")
-	a.WriteString(`"text":"`)
-	a.WriteString(str)
-	a.WriteString(`"}`)
-	 
-    queryStr :=a.String()
-	var b bytes.Buffer
-	b.WriteString("https://aip.baidubce.com/rpc/2.0/nlp/v1/lexer?access_token=")
-	b.WriteString(tk)
-
-	resp, err := http.Post(b.String(), "application/json", strings.NewReader(queryStr))
-	if err != nil {
-		log.Println("Get server response err:",err)
-	}
+	resp, _ := http.PostForm(TOKEN_API_URI, postArgs)
 	defer resp.Body.Close()
-	respRst := new(wordResult)
-	body, err := ioutil.ReadAll(resp.Body)
-	ostr := mahonia.NewDecoder("gbk").ConvertString(string(body))
-	log.Println(ostr)
-	if strings.Contains(ostr, `"error_code":110`) || strings.Contains(ostr, `"error_code":111`) {
-	errc := errors.New("bad token")
-	return respRst, errc	
+	newAccessToken := new(accessTokenResponse)
+	if err := json.NewDecoder(resp.Body).Decode(newAccessToken); err != nil {
+		panic(err)
 	}
-	
-	if err = json.Unmarshal([]byte(ostr),&respRst); err != nil {
-		log.Println("Parse response to json err:",err)
+
+	if newAccessToken.Error != "" {
+		panic(errors.New(newAccessToken.Error + ": " + newAccessToken.ErrorDescription))
 	}
-	return respRst, nil
-	
-}
 
-
-type address struct {
- AddType string                  `json:"type"`
- ByteOffset int64                `json:"byte_offset"`
- ByteLength int64                `json:"byte_length"`
-}
-type wordItem struct {
- Item string                     `json:"item"`
- Ne string                       `json:"ne"`
- Pos string                      `json:"pos"`
- ByteOffset int64                `json:"byte_offset"`
- ByteLength int64                `json:"byte_length"`
- Uri string                      `json:"uri"`
- Formal string                   `json:"formal"`
- BasicWord []string              `json:"base_words"`
- LocalDetails []address          `json:"loc_details"`
-}
-type wordResult struct {
- Text string                      `json:"text"`
- Items []wordItem                 `json:"items"`
+	return newAccessToken
 }
 
 func main() {
-    cfg := new(Config)
-	cfg.ApiKey = "bwG08GfxuwBIhIjTGtC3e4na"
-	cfg.SecretKey = "lhPE5TjKKT0VXVDGMvF3WGuRao4SgMuh"
-	
-	istrc := "山东泰安泰山区泰玻大街中段峪龙佳苑55号楼3单元1603，周京成收，15853873773"
-	
-	
-	tokenb, err := ioutil.ReadFile("token.cfg")
-    if err != nil {
-	     
-        log.Println("Get token from file err:",err)
-    }
-    token := string(tokenb)
-	
-	respRst2, err := analyzeWords(token, istrc)
+
+	configFileData, err := ioutil.ReadFile("token.json")
 	if err != nil {
-	log.Println(err)
-	accessToken, err := getToken(cfg)
+		return
+	}
+	err = json.Unmarshal(configFileData, currentAccessToken)
 	if err != nil {
-	log.Println(err)
+		return
 	}
-	token = accessToken
-	err = ioutil.WriteFile("token.cfg", []byte(token), 0644)
-	respRst2, err = analyzeWords(token, istrc)
-	}
-	log.Println(respRst2.Text)
-	
+	log.Println("current_token:", currentAccessToken.AccessToken)
+
+	aa := OcrTable("1.jpg")
+	fmt.Println(aa)
+
 }
+
+// {"refresh_token":"25.b4347b49afe043a1f523e870df05bb9d.315360000.1871566213.282335-16114169","expires_in":2592000,"session_key":"9mzdDxUrfQTp3AR+9JQvDcZWZ8yyf5zWvluGlx\/2\/x9Mv\/ggDxUMadbCRETNNIevKIVAaBnCEojq5i289TBBy+lojv0iNg==","access_token":"24.4127187fdf9e41c2f7e27541bf20f076.2592000.1558798213.282335-16114169","scope":"public vis-ocr_ocr brain_ocr_scope brain_ocr_general brain_ocr_general_basic brain_ocr_general_enhanced vis-ocr_business_license brain_ocr_webimage brain_all_scope brain_ocr_idcard brain_ocr_driving_license brain_ocr_vehicle_license vis-ocr_plate_number brain_solution brain_ocr_plate_number brain_ocr_accurate brain_ocr_accurate_basic brain_ocr_receipt brain_ocr_business_license brain_solution_iocr brain_ocr_handwriting brain_ocr_passport brain_ocr_vat_invoice brain_numbers brain_ocr_train_ticket brain_ocr_taxi_receipt vis-ocr_\u8f66\u8f86vin\u7801\u8bc6\u522b vis-ocr_\u5b9a\u989d\u53d1\u7968\u8bc6\u522b brain_ocr_vin brain_ocr_quota_invoice wise_adapt lebo_resource_base lightservice_public hetu_basic lightcms_map_poi kaidian_kaidian ApsMisTest_Test\u6743\u9650 vis-classify_flower lpq_\u5f00\u653e cop_helloScope ApsMis_fangdi_permission smartapp_snsapi_base iop_autocar oauth_tp_app smartapp_smart_game_openapi oauth_sessionkey smartapp_swanid_verify smartapp_opensource_openapi smartapp_opensource_recapi","session_secret":"5b662f207c31662ef88387387bbaec6d"}
---
+++ ocr.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+var ocrErrorCode = map[float64]string{
+	100: "无效参数",
+	110: "Token过期失效",
+}
+
+var ocrTypeCode = map[float64]string{
+	216015: "模块关闭",
+	216100: "非法参数",
+	216101: "参数数量不够",
+	216102: "业务不支持",
+	216103: "参数太长",
+	216110: "ID不存在",
+	216111: "非法用户ID",
+	216200: "空的图片",
+	216201: "图片格式错误",
+	216202: "图片大小错误",
+	216300: "DB错误",
+	216400: "后端系统错误",
+	216401: "内部错误",
+	216500: "未知错误",
+	216600: "身份证的ID格式错误",
+	216601: "身份证的ID和名字不匹配",
+	216611: "用户不存在",
+	216613: "用户查找不到",
+	216614: "图片信息不完整",
+	216615: "处理图片信息失败",
+	216616: "图片已存在",
+	216617: "添加用户失败",
+	216618: "群组里没有用户",
+	216630: "识别错误",
+	216631: "识别银行卡错误",
+}
+
+/**
+ * 身份证识别
+ */
+func OcrIdCard(imgPath string, isFront bool) map[string]interface{} {
+	imgbytes, err := ioutil.ReadFile(imgPath)
+	if err != nil {
+		log.Println(err)
+	}
+	postArgs := url.Values{}
+	postArgs.Set("access_token", currentAccessToken.AccessToken)
+	postArgs.Set("image", base64.StdEncoding.EncodeToString(imgbytes))
+	postArgs.Set("id_card_side", "front")     // front 正面  back 背面
+	postArgs.Set("detect_direction", "false") // 是否检测图像朝向[true/false]，默认不检测，即：false。朝向是指输入图像是正常方向、逆时针旋转90/180/270度。
+
+	if !isFront {
+		postArgs.Set("id_card_side", "back")
+	}
+
+	resp, _ := http.PostForm(IDCARD_API_URI, postArgs)
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if nil != err {
+		panic(err)
+	}
+
+	map_result := make(map[string]interface{})
+	json.Unmarshal(data, &map_result)
+
+	error_msg, ok := map_result["error_msg"]
+	if ok {
+		panic(error_msg)
+	}
+
+	return map_result
+}
+
+/**
+ * 身份证识别
+ */
+func OcrBankCard(imgPath string) map[string]interface{} {
+	imgbytes, err := ioutil.ReadFile(imgPath)
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+	postArgs := url.Values{}
+	postArgs.Set("access_token", currentAccessToken.AccessToken)
+	postArgs.Set("image", base64.StdEncoding.EncodeToString(imgbytes))
+
+	resp, _ := http.PostForm(BANKCARD_API_URI, postArgs)
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if nil != err {
+		panic(err)
+	}
+
+	map_result := make(map[string]interface{})
+	json.Unmarshal(data, &map_result)
+
+	error_msg, ok := map_result["error_msg"]
+	if ok {
+		panic(error_msg)
+	}
+
+	return map_result
+}
+
+/**
+ * 通用ocr识别
+ */
+func OcrGeneral(imgPath string) map[string]interface{} {
+	imgbytes, err := ioutil.ReadFile(imgPath)
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+
+	postArgs := url.Values{}
+	postArgs.Set("access_token", currentAccessToken.AccessToken)
+	postArgs.Set("image", base64.StdEncoding.EncodeToString(imgbytes))
+	postArgs.Set("recognize_granularity", "big")  // 是否定位单字符位置，big：不定位单字符位置，默认值；small：定位单字符位置
+	postArgs.Set("mask", "")                      // 是否检测图像朝向[true/false]，默认不检测，即：false。朝向是指输入图像是正常方向、逆时针旋转90/180/270度。
+	postArgs.Set("language_type", "CHN_ENG")      // CHN_ENG：中英文混合； ENG：英文； POR：葡萄牙语； FRE：法语； GER：德语； ITA：意大利语； SPA：西班牙语； RUS：俄语； JAP：日语
+	postArgs.Set("detect_direction", "false")     // 是否检测图像朝向[true/false]，默认不检测，即：false。朝向是指输入图像是正常方向、逆时针旋转90/180/270度。
+	postArgs.Set("detect_language", "false")      // 是否检测语言，默认不检测。当前支持（中文、英语、日语、韩语）
+	postArgs.Set("classify_dimension", "lottery") // 分类维度（根据OCR结果进行分类）
+	postArgs.Set("vertexes_location", "false")    //是否返回文字外接多边形顶点位置
+
+	resp, _ := http.PostForm(GENERALOCR_API_URI, postArgs)
+
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if nil != err {
+		panic(err)
+	}
+
+	map_result := make(map[string]interface{})
+	json.Unmarshal(data, &map_result)
+
+	error_msg, ok := map_result["error_msg"]
+	if ok {
+		panic(error_msg)
+	}
+
+	return map_result
+}
+
+func OcrTable(imgPath string) string {
+	imgbytes, err := ioutil.ReadFile(imgPath)
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+	postArgs1 := url.Values{}
+	postArgs1.Set("access_token", currentAccessToken.AccessToken)
+	postArgs1.Set("image", base64.StdEncoding.EncodeToString(imgbytes))
+
+	resp, _ := http.PostForm(TABLE_API_REQ_URI, postArgs1)
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if nil != err {
+		panic(err)
+	}
+
+	map_result := make(map[string]interface{})
+	json.Unmarshal(data, &map_result)
+	requestID := ""
+	if v, ok := map_result["result"].([]interface{})[0].(map[string]interface{}); ok {
+		requestID = v["request_id"].(string)
+
+	}
+
+	postArgs2 := url.Values{}
+	postArgs2.Set("access_token", currentAccessToken.AccessToken)
+	postArgs2.Set("request_id", requestID)
+
+	downLoadUrl := "http://aaa.bbb.ccc"
+	for range time.Tick(5000 * time.Millisecond) {
+
+		resp, _ := http.PostForm(TABLE_API_RESP_URI, postArgs2)
+		defer resp.Body.Close()
+		data2, err := ioutil.ReadAll(resp.Body)
+		if nil != err {
+			panic(err)
+		}
+		map_result2 := make(map[string]interface{})
+		json.Unmarshal(data2, &map_result2)
+
+		if vv, ok := map_result2["result"].(map[string]interface{}); ok {
+			if vv["ret_code"].(float64) == 3 {
+				downLoadUrl = vv["result_data"].(string)
+				break
+			}
+
+		}
+
+	}
+
+	return downLoadUrl
+}
